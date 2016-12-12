c5c3844cb772abee82bd3863bc05c1c287d10a2f
Aleks New Work <new0808@yandex.ru>
2016-12-12 17:48:40 +0300

    Задача "светофор" в процессе.

--- js-solutions/lecture_7/traffic.light.js
+++ js-solutions/lecture_7/traffic.light.js
@@ -2,4 +2,74 @@
 
 'use strict'
 
+var timeRed, timeYellow, timeGreen,
+	lightRed 	= document.querySelector('.first'),
+	lightYellow = document.getElementsByClassName('second'),
+	lightGreen 	= document.getElementsByClassName('third'),
+	button 		= document.getElementById('turnTrafficLight'),
+	list;
+
+	list = lightRed.classList;
+
+	alert(list);
+
+	lightYellow[0].classList;
+
+	alert(list);
+
+	
+timeRed = document.getElementById('timeRed').value;
+
+alert(timeRed);
+
+
+function turnTrafficLight(){
+
+	while(true) {
+
+		var timeRed 	= document.getElementById('timeRed').value,
+			timeYellow	= document.getElementById('timeYellow').value,
+			timeGreen	= document.getElementById('timeGreen').value;
+
+			timeRed 	= parseFloat(timeRed);
+			timeYellow 	= parseFloat(timeYellow);
+			timeGreen 	= parseFloat(timeGreen);
+
+		var intervalRed = setInterval(function() {
+
+
+
+				}, 1000
+			);
+
+
+
+		if() { break; };
+
+
+	};
+
+};
+
+
+
+
+	button.onclick = function() {
+		
+		var time = 5;
+		button.disabled = true;
+		button.innerHTML = 'Разморозится через ' + time;
+
+  		var interval_id = setInterval(function() {  /* запуск таймера */
+    		time--;
+    		if (time != 0) {
+      			button.innerHTML = 'Разморозится через ' + time;
+    		} else {
+    			clearInterval(interval_id);  /* остановка таймера */
+      			button.disabled = false;
+      			button.innerHTML = 'Заморозить на 5 секунд';
+    		}
+  		}, 1000);
+	};
+	
 
